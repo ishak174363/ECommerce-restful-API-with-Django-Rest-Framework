e31030190ad4cd1805d5fadfa6862ef34ddfa97d
ishak174363 <ishaksheikh174363@gmail.com>
2024-03-04 17:18:45 +0600

    filter return single product

--- main/product/models.py
+++ main/product/models.py
@@ -29,11 +29,21 @@ class Brand(models.Model):
 #Product model created here
 class Product(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    slug=models.SlugField(max_length=250)
     description = models.TextField()
     is_digital= models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE) #on_delete=models.SET
     category = models.ForeignKey("Category", null=True, blank=True, on_delete=models.SET_NULL)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
    
+
+#ProductLine model created here
+class ProductLine(models.Model):
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    sku=models.CharField(max_length=50)
+    stock_qty=models.IntegerField()
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    is_active=models.BooleanField(default=False)
