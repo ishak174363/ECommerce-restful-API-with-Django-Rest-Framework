7764324b0c6f9d0e5c6c863f71cb9d3c93c3c313
ishak174363 <ishaksheikh174363@gmail.com>
2024-03-05 14:10:19 +0600

    Serializer Field Name Mapping and Flattening

--- main/product/serializers.py
+++ main/product/serializers.py
@@ -1,41 +1,36 @@
 from rest_framework import serializers
-from .models import Brand, Category, Product,ProductLine
+from .models import Brand, Category, Product, ProductLine
 
 
-
-#Create Category Serializer
+# Create Category Serializer
 class CategorySerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='name')
+
     class Meta:
         model = Category
-        #fields = '__all__'
-        fields=['name']
-        #exclude=['id']
+        fields = ['category_name']
 
 
-#Create Brand Serializer
+# Create Brand Serializer
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
-        #fields = '__all__'
-        exclude=['id']
+        exclude = ['id']
 
 
-#Create ProductLine Serializer
+# Create ProductLine Serializer
 class ProductLineSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductLine
-        #fields = '__all__'
-        exclude=['id']
-    
-    
-#Create Product Serializer
+        exclude = ['id']
+
+
+# Create Product Serializer
 class ProductSerializer(serializers.ModelSerializer):
-    brand=BrandSerializer()
-    category=CategorySerializer()
-    product_line=ProductLineSerializer(many=True)
-    
+    brand_name = serializers.CharField(source='brand.name') 
+    category_name = serializers.CharField(source='category.name')
+    product_line = ProductLineSerializer(many=True)
+
     class Meta:
         model = Product
-        #fields = '__all__'
-        exclude=['id']
-
+        fields = ('name', 'slug', 'description', 'brand_name', 'category_name', 'product_line')
